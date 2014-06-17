b0afc2a2a975a14b240f2f1907b5fcd47f530976
Callum027 <callum@huttradio.co.nz>
2014-06-18 10:12:33 +1200

    Use the right packages/service for mariadb-galera

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,17 +4,20 @@ class mysql::params
     'oracle'  => [ 'mysql-server' ],
     'percona' => [ 'percona-server-server-5.6' ],
     'mariadb' => [ 'mariadb-server-5.5' ],
+    'mariadb-galera'  => [ 'mariadb-galera-server-5.5' ],
   }
 
   $packages_extra = $mysql::type ? {
     'oracle'  => [  ],
     'percona' => [ 'xtrabackup' ],
     'mariadb' => [ ],
+    'mariadb-galera'  => [ 'galera' ],
   }
 
   $service = $mysql::type ? {
     'oracle'  => [ 'mysql' ],
     'percona' => [ 'mysql' ],
-    'mariadb' => [ 'mysql' ],
+    /mariadb(-galera)?/ => [ 'mysql' ],
+    
   }
 }
